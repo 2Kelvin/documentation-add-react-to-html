14ffcd161fd34485c713049658a4635a3018b3e7
2Kelvin <kvin.mwangi254@gmail.com>
2022-10-09 18:19:20 +0300

    added another react root

--- likeButton.js
+++ likeButton.js
@@ -17,3 +17,8 @@ function likeButton() {
 const rootNode = document.getElementById("likeButtonRoot");
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(likeButton));
+
+// for anotherRoot div 
+const anotherRootNode = document.getElementById("anotherRoot");
+const anotherRoot = ReactDOM.createRoot(anotherRootNode);
+anotherRoot.render(React.createElement(likeButton));
